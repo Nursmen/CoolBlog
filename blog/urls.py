b0b2d3b55e4bf796f93da34d919ebdc5892179be
Nursmen <nnursultan07@mail.ru>
2022-03-09 11:40:28 +0600

    Create post_details

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='posts'),
+    path('post#<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    post = Post.objects.exclude(published_date = None).\
+        filter(published_date__lte=timezone.now()).\
+        order_by('published_date')
     # means published_date <= timezone.now() (меньше или равно)
     # just QuerySet 
 
     return render(request, 'blog/post_list.html', {'posts':post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
