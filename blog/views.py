68cb9638c425377a007800badd0642084f914ab3
Nursmen <nnursultan07@mail.ru>
2022-03-09 09:30:23 +0600

    Create norm html view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # means published_date <= timezone.now() (меньше или равно)
+    # just QuerySet 
+
+    return render(request, 'blog/post_list.html', {'posts':post})
